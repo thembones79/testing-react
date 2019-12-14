28262863309f553bb0eb2e61b7a3f1c1b1e8959b
thembones79 <michal.szumnarski@gmail.com>
2019-12-14 22:23:23 +0100

    introduced piece of state

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,3 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => <div>Comment Box</div>;
+class CommentBox extends Component {
+  state = { comment: "" };
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // TODO - Call an action creator
+    // And save the comment
+
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
