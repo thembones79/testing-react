4fd72d0d1f276a2f070e2fa3e49ee52156092973
thembones79 <michal.szumnarski@gmail.com>
2019-12-16 17:25:50 +0100

    added form test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -21,4 +21,12 @@ it("has a text area that users can type in", () => {
   wrapped.find("textarea").simulate("change", {
     target: { value: "new comment" }
   });
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("has emptied textarea after form submission", () => {
+  wrapped.find("form").simulate("submit", {});
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
 });
