cccc123e10921502f3ed623f9b63adb8df21dfc7
thembones79 <michal.szumnarski@gmail.com>
2019-12-17 23:59:44 +0100

    redux testing is working

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class CommentBox extends Component {
   state = { comment: "" };
@@ -11,6 +13,7 @@ class CommentBox extends Component {
 
     // TODO - Call an action creator
     // And save the comment
+    this.props.saveComment(this.state.comment);
 
     this.setState({ comment: "" });
   };
@@ -42,4 +45,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
