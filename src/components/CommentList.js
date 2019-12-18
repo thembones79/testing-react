e48566b353d7e17a1ea2ef9af7de6bcc29eca88d
thembones79 <michal.szumnarski@gmail.com>
2019-12-19 00:13:40 +0100

    added tests for comment list component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,3 +1,31 @@
-import React from "react"
+import React from "react";
+import { connect } from "react-redux";
 
-export default () => <div>Comment List</div>
+class CommentList extends React.Component {
+  renderComments = () => {
+    return this.props.comments.map(comment => {
+      return (
+        <li className="list-item" key={comment}>
+          <span className="icon  has-text-primary">
+            <i className="fas fa-chevron-right "> </i>
+          </span>{" "}
+          {comment}
+        </li>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul className="list">{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStatetoProps(state) {
+  return { comments: state.comments };
+}
+
+export default connect(mapStatetoProps)(CommentList);
